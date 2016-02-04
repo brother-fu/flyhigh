60163f1156b06500c7db211a0416a054200e1407
lwyrookie <lwyrookie@MacBook-Pro.local>
2016-02-04 00:45:16 -0500

    first commit

---
+++ google/DupOddRemoveEven.java
@@ -0,0 +1,10 @@
+/* this are for interview questions*/
+//http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=168618&extra=page%3D1%26filter%3Dsortid%26sortid%3D311%26sortid%3D311
+
+import java.io.*;
+import java.util.*;
+public class DupOddRemoveEven {
+	public void do (int[] num) {
+	
+	}
+}
